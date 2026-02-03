752b9fa153e2fb10d9623254740864e72c280294
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2026-02-03 16:20:38 +0100

    Added base project build with an scss reset

---
+++ actual_shopping_list_app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header></header>
+      <main>
+        <p>Testing</p>
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
